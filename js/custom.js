438896c9096deec5b04a1e1b19f61a1b7234552d
AK-Polash <polashk199@gmail.com>
2022-11-05 15:56:06 +0600

    scrolling event added here

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,43 @@
 let type = document.querySelector(".type");
 let text = type.innerHTML;
-let textArr = text.split("");
+let textArr;
 let count = 0;
+let runOneTime = true;
 type.innerHTML = "";
 
-function typeJs() {
-  if (count < text.length) {
-    type.innerHTML += text.charAt(count);
-    count++;
-    textArr = text.split("");
-  } else {
-    if (textArr.length > 0) {
-      textArr.pop();
-      let convertedText = textArr.join("");
-      type.innerHTML = convertedText;
-    } else {
-      count = 0;
+window.addEventListener("scroll", () => {
+  let viewPort = type.getBoundingClientRect();
+  if (isInViewPort() && runOneTime) {
+    runOneTime = false;
+    function typeJs() {
+      if (count < text.length) {
+        type.innerHTML += text.charAt(count);
+        count++;
+        textArr = text.split("");
+      } else {
+        if (textArr.length > 0) {
+          textArr.pop();
+          let convertedText = textArr.join("");
+          type.innerHTML = convertedText;
+        } else {
+          count = 0;
+        }
+      }
     }
-  }
-  console.log(count);
-}
 
-setInterval(() => {
-  typeJs();
-}, 300);
+    setInterval(() => {
+      typeJs();
+    }, type.dataset.time);
+  }
+
+  function isInViewPort() {
+    return (
+      viewPort.top >= 0 &&
+      viewPort.left >= 0 &&
+      viewPort.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      viewPort.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+});
