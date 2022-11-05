f6bcf7e2a1449ceacf22bca985823adf79d8e921
AK-Polash <polashk199@gmail.com>
2022-11-06 00:38:43 +0600

    multiple time use ability added here
    now we can use multiple typeJS in  a single page.
    Though I'm on doubt about the scoping of "currentIndex" in js code which needs to have a clear vision.

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,52 @@
-let type = document.querySelector(".type");
-let text = type.innerHTML;
-let textArr;
-let count = 0;
-let runOneTime = true;
-type.innerHTML = "";
+let type = document.querySelectorAll(".type");
+let typeArr = Array.from(type);
+let currentIndex;
 
-window.addEventListener("scroll", () => {
-  let viewPort = type.getBoundingClientRect();
-  if (isInViewPort() && runOneTime) {
-    runOneTime = false;
-    function typeJs() {
-      if (count < text.length) {
-        type.innerHTML += text.charAt(count);
-        count++;
-        textArr = text.split("");
-      } else {
-        if (textArr.length > 0) {
-          textArr.pop();
-          let convertedText = textArr.join("");
-          type.innerHTML = convertedText;
+typeArr.map((item, index) => {
+  let text = typeArr[index].innerHTML;
+  let textArr;
+  let count = 0;
+  let runOneTime = true;
+  item.innerHTML = "";
+  console.log(currentIndex);
+  // eikhane "currentIndex" undefined dekhay kano...? "post_share" project e same way te currentIndex ke globaly scope korsilam tokhon kaj korsilo
+
+  window.addEventListener("scroll", () => {
+    currentIndex = index;
+    let viewPort = typeArr[index].getBoundingClientRect();
+
+    if (isInViewPort() && runOneTime) {
+      runOneTime = false;
+      function typeJs() {
+        if (count < text.length) {
+          typeArr[index].innerHTML += text.charAt(count);
+          count++;
+          textArr = text.split("");
         } else {
-          count = 0;
+          if (textArr.length > 0) {
+            textArr.pop();
+            let convertedText = textArr.join("");
+            typeArr[index].innerHTML = convertedText;
+          } else {
+            count = 0;
+          }
         }
       }
+
+      setInterval(() => {
+        typeJs();
+      }, typeArr[index].dataset.time);
     }
 
-    setInterval(() => {
-      typeJs();
-    }, type.dataset.time);
-  }
-
-  function isInViewPort() {
-    return (
-      viewPort.top >= 0 &&
-      viewPort.left >= 0 &&
-      viewPort.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      viewPort.right <=
-        (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+    function isInViewPort() {
+      return (
+        viewPort.top >= 0 &&
+        viewPort.left >= 0 &&
+        viewPort.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        viewPort.right <=
+          (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  });
 });
